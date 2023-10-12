9da296b7ac82554f94c14bb0740e9d6b5ed94ca3
venturions <alex.eduardo10@hotmail.com>
2023-10-12 19:19:41 -0300

    Recuperar as métricas de um usuário

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -13,6 +13,68 @@ export async function usersRoutes(app: FastifyInstance) {
     return { users }
   })
 
+  app.get(
+    '/metrics',
+    { preHandler: [CheckSessionIdExists] },
+    async (request, response) => {
+      const { sessionId } = request.cookies
+
+      const meals = await knex('meals')
+        .select(
+          'meals.id',
+          'meals.user_id',
+          'meals.name',
+          'meals.description',
+          'meals.dateAndHour',
+          'meals.inDiet',
+        )
+        .innerJoin('users', 'users.id', 'meals.user_id')
+        .where('session_id', sessionId)
+
+      if (meals.length === 0) {
+        return response.status(404).send({
+          message:
+            'Você não tem nenhuma refeição cadastrada para obter métricas.',
+        })
+      }
+
+      const totalNumberOfMeals = meals.length
+      const totalNumberOfMealsInTheDiet = meals.filter(
+        (item) => item.inDiet === 1,
+      ).length
+      const totalNumberOfMealsOffTheDiet = meals.filter(
+        (item) => item.inDiet === 0,
+      ).length
+
+      let currentStreak = 0
+      let maxStreak = 0
+
+      for (const meal of meals) {
+        if (meal.inDiet === 1) {
+          currentStreak++
+        } else {
+          if (currentStreak > maxStreak) {
+            maxStreak = currentStreak
+          }
+          currentStreak = 0
+        }
+      }
+
+      if (currentStreak > maxStreak) {
+        maxStreak = currentStreak
+      }
+
+      const metricsResponse = {
+        totalNumberOfMeals,
+        totalNumberOfMealsInTheDiet,
+        totalNumberOfMealsOffTheDiet,
+        bestSequenceOfMealsWithinTheDiet: maxStreak,
+      }
+
+      return metricsResponse
+    },
+  )
+
   app.post('/', async (request, response) => {
     const createUserBodySchema = z.object({
       name: z.string(),
